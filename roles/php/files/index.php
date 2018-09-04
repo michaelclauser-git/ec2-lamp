e47405a1b68c7468b087e7253b660aa882d00da0
Mike Clauser <m.clauser@comcast.net>
2018-09-03 23:45:42 -0400

    update to test cronjob

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -5,5 +5,7 @@
 </head>
 <body>
 	<h1><?php echo "Welcom to DevOps on AWS. This text was generated using PHP 7" ?></h1>
+
+	<p><?php echo " Here is a stupid update to test the creation of the ansible pull feature and the cronjob we added to it" ?></p>
 </body>
 </html>
